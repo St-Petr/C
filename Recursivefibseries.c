afbd8f42e7b1e8fed64181fd8db0468901d0d7a4
St-Petr <112187138+St-Petr@users.noreply.github.com>
2023-01-04 22:04:29 +0100

    Recursions-simple

---
+++ Recursivefibseries.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int fibonacci(int n)
+{
+	if(n == 0)
+	{
+		return 0;
+		}
+		
+		if(n == 1)
+		{
+			return 1;
+			}
+			
+			return fibonacci(n-1) + fibonacci(n-2);
+			}
+			
+			int main()
+			{
+				int n;
+				for (n = 0; n < 10; n++)
+				{
+					printf("%d\t\n", fibonacci(n));
+					}
+					
+					return 0;
+					}
---
+++ recursivefactorial.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+unsigned long long int factorial(unsigned int n)
+{
+	if(n == 1)
+	{
+		return 1;
+	}
+	return n * factorial (n-1);
+	}
+	
+	int main()
+	{
+		int n = 4;
+		printf("Factorial of %d is %llu\n", n, factorial(n));
+		return 0;
+		}
